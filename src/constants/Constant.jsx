bf5a76fd893cb77fe3346f93795cd9a69a2fd424
Vivek Kumar <vivekkumarjnv054@gmail.com>
2025-03-25 17:14:57 +0530

    Fixed image path and case-sensitivity issues

--- src/constants/Constant.jsx
+++ src/constants/Constant.jsx
@@ -1,32 +1,31 @@
+import BIET from "../assets/biet.png";
 import C from "../assets/C.png";
 import CPP from "../assets/CPP.png";
-import HTML from "../assets/HTML.png";
-import Github from "../assets/Github.png";
 import CSS from "../assets/CSS.png";
-import Python from "../assets/Python.png";
-import Javascript from "../assets/Javascript.png";
+import Educations from "../assets/education.png";
+import ExpressJs from "../assets/expressjs.png";
+import GDSC from "../assets/GDSC.png";
 import Git from "../assets/Git.png";
-import Vite from "../assets/Vite.png";
-import TailwindCSS from "../assets/TailwindCSS.png";
-import react from "../assets/React.png";
+import Github from "../assets/Github.png";
+import GPT4 from "../assets/gpt4.jpg";
+import GSSOC from "../assets/GSSOC.png";
 
 import Hackthechain from "../assets/HackTheChainLogo.png";
-import GSSOC from "../assets/GSSoc.jpg";
 
-import GDSC from "../assets/GDSC.png";
+import HTML from "../assets/HTML.png";
+import Javascript from "../assets/Javascript.png";
 import KDE from "../assets/kde.png";
-
-import GPT4 from "../assets/gpt4.jpg";
+import MongoDB from "../assets/mongodb.png";
+import NodeJs from "../assets/nodejs.png";
 import Portfolio from "../assets/portfolio.jpg";
+import Projects from "../assets/projects.png";
+import Python from "../assets/Python.png";
+import react from "../assets/React.png";
 import Rocket from "../assets/Rocket.jpg";
 import Game from "../assets/Stone.jpg";
+import TailwindCSS from "../assets/TailwindCSS.png";
+import Vite from "../assets/Vite.png";
 import WellBeing from "../assets/well-being.jpg";
-import BIET from "../assets/biet.png";
-import ExpressJs from "../assets/expressjs.png";
-import NodeJs from "../assets/nodejs.png";
-import MongoDB from "../assets/mongodb.png";
-import Projects from "../assets/projects.png";
-import Educations from "../assets/education.png";
 
 export {
   C,
